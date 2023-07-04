66cb7be62b5f22f3f9cc2bf1955fbacf4b877e0c
MahderZergaw <mahderzergaw640@gmail.com>
2023-07-04 11:42:37 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ *print_listint - print everyting
+ *@h:param 1
+ *Return:size_t
+ */
+size_t print_listint(const listint_t *h)
+{
+size_t i = 0;
+while (h)
+{
+printf("%d\n", h->n);
+h = h->next;
+i++;
+}
+return (i);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This is README for more singlely linked list
