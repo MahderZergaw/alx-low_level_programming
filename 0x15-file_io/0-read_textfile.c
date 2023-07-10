5ad5136cc500e3c6833b65021327a3d29dc2c688
MahderZergaw <mahderzergaw640@gmail.com>
2023-07-10 11:11:03 -0700

    read

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * read_textfile-function thta reads text file
+ * @filename:the name of the file
+ * @letters:param2
+ * Return: Size_t
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
+
+	free(buf);
+	close(fd);
+	return (w);
+}
